ebdd07d607c2a436a7fe674dec874d840a641fbc
steffafregele-oss <steffafregele@gmail.com>
2025-11-03 00:05:05 +0200

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Ticket Bot is alive ✅'));
+app.get("/", (req, res) => res.send("Bot is alive ✅"));
 
 app.listen(PORT, () => {
-  console.log(`🚀 Keep-alive server running on port ${PORT}`);
+  console.log(`✅ Keep-alive server running on port ${PORT}`);
 });
